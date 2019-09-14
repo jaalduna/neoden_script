e1ce9fa9c37a8d9bb33d3ff12cb2a0635117d431
joaquin <jaalduna@gmail.com>
2019-09-14 18:01:37 -0300

    scripts ready
    Only bottom left corner should be defined on the pnp software.

---
+++ pnp_bottom.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import math
+
+#read data from excel tables
+excel_file = 'pnp1.xls'
+components_table = pd.read_excel(excel_file,sheet_name='component setup')
+feeders_table = pd.read_excel(excel_file, sheet_name = 'feeder settings') 
+marks_table = pd.read_excel(excel_file,sheet_name = "board configurations")
+#print(components_table.head())
+#print feeders_table.head()
+
+#lets create csv file for pnp machine
+f_out_top = open("out_bottom.csv",'w')
+
+#write initial configs
+f_out_top.write("#initial configurations")
+f_out_top.write("pcb,Manual,Lock,100,100,Back,150,0,10,\n")
+f_out_top.write("test,No,\n")
+f_out_top.write("mirror_create,0,1,1,95.24,230.39,0,0,0,0,0,0,0,\n")
+f_out_top.write("mirror,95.24,230.39,0,No,\n")
+
+
+
+#write mark configuration
+f_out_top.write("#marks configuration")
+f_out_top.write("Mark recogniseRecognise typeManualIdentification Range of Placement Head\n")
+f_out_top.write("markconfirm,Mirror,Auto,0,\n")
+
+board_dim_x = 0;
+board_dim_y = 0;
+
+for row in marks_table.itertuples():
+    if(row.type == "dim"):
+            board_dim_x = row.x;
+            board_dim_y = row.y;
+    if(row.type == "mark"):
+        reg = "mark,"
+        reg += str(board_dim_x- row.x) + ","
+        reg += str(row.y) +","
+        reg += "{0:.3f}".format(row._4 - 0.3)+","
+        reg += "{0:.3f}".format(row._4 + 0.5)+","
+        reg += "1,75,\n"
+        f_out_top.write(reg)
+
+
+#write feeder settings
+f_out_top.write("#feeder settings \n")
+for row in feeders_table.itertuples():
+    #the fields for each register are:
+    #Feeder, Feeder ID
+    reg = ""
+    for columns in row[1:]:
+        if(columns != columns):
+            reg +=","
+        else:
+            reg += str(columns)+","
+    reg +="\n"
+    f_out_top.write(reg)
+    #print row
+
+    #skip one line
+f_out_top.write("\n")
+
+#write components setup
+f_out_top.write("#components setup\n")
+f_out_top.write("comp,1,1,dummy,dummy,dummy,0,0,0,Yes,\n")
+for row in components_table.itertuples():
+    #the fields for each register are:
+    #Stack, Nozzle, Name, Value, Footprint, _6, _7, Rotation, skip
+    reg = "comp,"
+    if(row.Layer == 'BottomLayer'):
+      
+        for columns in row[1:6]:
+
+            reg += str(columns)+","
+            #don't write the Layer column
+        reg +=  "{0:.3f}".format(board_dim_x - row._6)+","
+        for columns in row[7:9]:
+            reg += str(columns)+","
+            
+        reg += str(row.skip)+", \n"
+        f_out_top.write(reg)
+
+
+
+
+
